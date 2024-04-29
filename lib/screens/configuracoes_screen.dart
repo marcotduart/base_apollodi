83916be13d4f4179d0b5c34c4e627331aceb9e97
Marco Antônio <marcotdantonio@gmail.com>
2024-04-29 13:38:48 -0300

    3.0

--- lib/screens/configuracoes_screen.dart
+++ lib/screens/configuracoes_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter/services.dart'; // Import needed for input formatters
+import 'package:flutter/services.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 class ConfiguracoesScreen extends StatelessWidget {
   @override
@@ -21,7 +22,62 @@ class ConfiguracoesScreen extends StatelessWidget {
   }
 }
 
-class ConnectionSection extends StatelessWidget {
+class ConnectionSection extends StatefulWidget {
+  @override
+  _ConnectionSectionState createState() => _ConnectionSectionState();
+}
+
+class _ConnectionSectionState extends State<ConnectionSection> {
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  BluetoothDevice? connectedDevice;
+  List<BluetoothService> services = [];
+
+  @override
+  void initState() {
+    super.initState();
+    startScan();
+  }
+
+  void startScan() {
+    flutterBlue.startScan(timeout: Duration(seconds: 4));
+
+    // Listen to scan results
+    var subscription = flutterBlue.scanResults.listen((results) {
+      for (ScanResult result in results) {
+        print('${result.device.name} encontrado! rssi: ${result.rssi}');
+        if (result.device.name == 'ESP32') {
+          connectToDevice(result.device);
+          flutterBlue.stopScan();
+          break;
+        }
+      }
+    });
+
+    Future.delayed(Duration(seconds: 4), () {
+      flutterBlue.stopScan();
+      subscription.cancel();
+    });
+  }
+
+  void connectToDevice(BluetoothDevice device) async {
+    await device.connect();
+    setState(() {
+      connectedDevice = device;
+    });
+
+    discoverServices();
+  }
+
+  void discoverServices() async {
+    if (connectedDevice != null) {
+      List<BluetoothService> services =
+          await connectedDevice!.discoverServices();
+      setState(() {
+        this.services = services;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -29,12 +85,16 @@ class ConnectionSection extends StatelessWidget {
         children: <Widget>[
           ListTile(
             leading: Icon(Icons.bluetooth),
-            title: Text('Conectar à Base de Lançamento'),
-            subtitle: Text('Status da conexão com a base: [DESATIVADO]'),
+            title: Text('Conexão com Base de Lançamento'),
+            subtitle:
+                Text(connectedDevice != null ? 'Conectado' : 'Desconectado'),
           ),
           ListTile(
-            title: Text('Tempo de conexão: 5 ms'),
-            subtitle: Text('Escanenado dispositivos...'),
+            title: Text(
+                'Tempo de conexão: ${connectedDevice != null ? 'Ativo' : 'Desativado'}'),
+            subtitle: Text(connectedDevice != null
+                ? 'Conectado com sucesso!'
+                : 'Escanenado dispositivos...'),
           ),
         ],
       ),
@@ -67,14 +127,15 @@ class ParametersSection extends StatelessWidget {
           children: <Widget>[
             TextField(
               controller: inclinacaoController,
-              decoration: InputDecoration(labelText: 'Ângulo de Inclinação'),
+              decoration:
+                  InputDecoration(labelText: 'Ângulo de Inclinação (º)'),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
             ),
             SizedBox(height: 10),
             TextField(
               controller: pressaoController,
-              decoration: InputDecoration(labelText: 'Pressão Máxima'),
+              decoration: InputDecoration(labelText: 'Pressão Máxima (psi)'),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
             ),
